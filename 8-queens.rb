5ed8aa0df301555aedb1a252348091efa7fc1e04
App Academy Student <appacademy@Apps-Mac-mini-77.local>
2016-08-26 11:04:44 -0400

    Finish helper methods

--- 8-queens.rb
+++ 8-queens.rb
@@ -1,23 +1,25 @@
 require 'colorize'
+require 'byebug'
 
 class EightQueens
   def self.empty_grid
     grid = Array.new(8) { Array.new(8) }
-    grid [5][4] = 'Q'
-    grid [7][6] = 'Q'
-    grid
   end
 
-  def solve(grid = EightQueens.empty_grid)
+  def solve(grid = EightQueens.empty_grid, row = 0, col = 0)
+    return false if grid.has_error?
+    return true if row > 7 ## solved!
+
+    ## iterate over possibilities
+
 
   end
 
   def has_error?(grid)
-    row_error?(grid) || col_error?(grid) #|| diag_error?(grid)
+    row_error?(grid) || col_error?(grid) || diag_error?(grid)
   end
 
   def row_error?(grid)
-    render(grid)
     grid.each do |row|
       q_count = 0
       row.each do |space|
@@ -33,15 +35,30 @@ class EightQueens
     grid.each_with_index do |row, row_num|
       q_count = 0
       row.each_with_index do |space, col_num|
-        q_count += 1 unless grid[row_num, col_num].nil?
+        q_count += 1 unless grid[row_num][col_num].nil?
       end
-      return true if q_count > 1
       q_count = 0
     end
     false
   end
 
   def diag_error?(grid)
+    grid.each_with_index do |row, row_num|
+      row.each_with_index do |space, col_num|
+        deltas = [[-1, 1], [1, 1] [-1, 1], [-1, -1]]
+        deltas.each do |delta|
+          delta_row = delta.first
+          delta_col = delta.last
+          search_pos_row = row_num + delta_row
+          search_pos_col = col_num + delta_col
+          while search_pos_col.between?(0, 7) && search_pos_row.between?(0, 7)
+            return true unless grid[search_pos_row][search_pos_col].nil?
+            search_pos_row += delta_row
+            search_pos_col += delta_col
+          end
+        end
+      end
+    end
     false
   end
 
@@ -76,4 +93,4 @@ class EightQueens
   end
 end
 
-p EightQueens.new.has_error?(EightQueens.empty_grid)
+p EightQueens.new.solve(EightQueens.empty_grid)
