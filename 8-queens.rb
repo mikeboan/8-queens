0751d3a70d84b0b0d58cd00c2e96f16db7b81f4a
Mike Boan <mikeboan@gmail.com>
2016-08-26 22:08:17 -0400

    Implement recursive solve function, potentially with bugs

--- 8-queens.rb
+++ 8-queens.rb
@@ -2,25 +2,59 @@ require 'colorize'
 require 'byebug'
 
 class EightQueens
-  def self.empty_grid
-    grid = Array.new(8) { Array.new(8) }
+  def self.empty_board
+    board = Array.new(8) { Array.new(8) }
+    # board[0][2] = "Q"
+    # board[1][5] = "Q"
+    # board[2][3] = "Q"
+    # board[3][0] = "Q"
+    # board[4][7] = "Q"
+    # board[5][4] = "Q"
+    # board[5][4] = "Q"
+    # board[5][4] = "Q"
+    # board[6][6] = "Q"
+    board
   end
 
-  def solve(grid = EightQueens.empty_grid, row = 0, col = 0)
-    return false if grid.has_error?
+  def solve(board = EightQueens.empty_board, row = 0, col = 0)
+    # debugger
+    return false if has_error?(board)
     return true if row > 7 ## solved!
 
+    # sleep(3.0)
+
     ## iterate over possibilities
+    8.times do |new_row|
+      # debugger
+      place_queen(board, new_row, col)
+      # debugger
+      if solve(board, new_row, col + 1)
+        render(board)
+        return true
+      end 
+      remove_queen(board, new_row, col)
+    end
 
-
+    ## return false if
+    false
   end
 
-  def has_error?(grid)
-    row_error?(grid) || col_error?(grid) || diag_error?(grid)
+  def place_queen(board, row, col)
+    board[row][col] = "Q"
   end
 
-  def row_error?(grid)
-    grid.each do |row|
+  def remove_queen(board, row, col)
+    board[row][col] = nil
+  end
+
+  def has_error?(board)
+    render(board)
+    row_error?(board) || col_error?(board) || diag_error?(board)
+  end
+
+  def row_error?(board)
+    # debugger
+    board.each do |row|
       q_count = 0
       row.each do |space|
         q_count += 1 unless space.nil?
@@ -31,20 +65,23 @@ class EightQueens
     false
   end
 
-  def col_error?(grid)
-    grid.each_with_index do |row, row_num|
+  def col_error?(board)
+    # debugger
+    board.each_with_index do |row, row_num|
       q_count = 0
       row.each_with_index do |space, col_num|
-        q_count += 1 unless grid[row_num][col_num].nil?
+        q_count += 1 unless board[row_num][col_num].nil?
       end
       q_count = 0
     end
     false
   end
 
-  def diag_error?(grid)
-    grid.each_with_index do |row, row_num|
+  def diag_error?(board)
+    # debugger
+    board.each_with_index do |row, row_num|
       row.each_with_index do |space, col_num|
+        next if board[row_num][col_num].nil?
         deltas = [[-1, 1], [1, 1] [-1, 1], [-1, -1]]
         deltas.each do |delta|
           delta_row = delta.first
@@ -52,7 +89,8 @@ class EightQueens
           search_pos_row = row_num + delta_row
           search_pos_col = col_num + delta_col
           while search_pos_col.between?(0, 7) && search_pos_row.between?(0, 7)
-            return true unless grid[search_pos_row][search_pos_col].nil?
+            # debugger
+            return true unless board[search_pos_row][search_pos_col].nil?
             search_pos_row += delta_row
             search_pos_col += delta_col
           end
@@ -62,13 +100,13 @@ class EightQueens
     false
   end
 
-  def render(grid)
+  def render(board)
     system("clear")
     puts "           8 QUEENS"
     print "   "
     ('a'..'h').each { |letter| print " " + letter + " " }
     puts
-    grid.each_with_index do |row, row_num|
+    board.each_with_index do |row, row_num|
       print " " + (8 - row_num).to_s + " "
 
       row.each_with_index do |piece, col_num|
@@ -93,4 +131,5 @@ class EightQueens
   end
 end
 
-p EightQueens.new.solve(EightQueens.empty_grid)
+board = EightQueens.empty_board
+p EightQueens.new.solve(board)
